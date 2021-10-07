663f759dc4b8027c4cc113b0acfd05e3efe9a7fc
Petros Toupas <ptoupas@gmail.com>
2021-10-07 04:49:55 +0300

    fully supported conv3d layer

--- fpgaHART/layers/base_layer.py
+++ fpgaHART/layers/base_layer.py
@@ -1,6 +1,7 @@
 import configparser
 import os
 import math
+import numpy as np
 
 class BaseLayer():
     def __init__(self, data_format='NDHWC'):
@@ -9,26 +10,69 @@ class BaseLayer():
         self.get_config()
         self.word_bytes = self.word_length/8
         self.cycles_per_sec = self.clock_freq*1e6
+        self.mem_bandwidth = self.mem_bw * 1e9
+        self.mem_words_per_cycle = (self.mem_bandwidth / self.word_length) / self.cycles_per_sec
 
     def get_config(self):
         config = configparser.ConfigParser()
         config.read(os.path.join(os.getcwd(), 'fpgaHART', 'config', 'config_fpga.ini'))
 
-        self.word_length = config.get('FPGA Specifications', 'word_length')
-        self.clock_freq = config.get('FPGA Specifications', 'clock_freq')
-        self.bram = config.get('FPGA Specifications', 'bram')
-        self.bram_bytes = config.get('FPGA Specifications', 'bram_type') / 8
-        self.dsp = config.get('FPGA Specifications', 'dsp')
-        self.mem_bw = config.get('FPGA Specifications', 'mem_bw')
+        self.word_length = int(config.get('FPGA Specifications', 'word_length'))
+        self.clock_freq = int(config.get('FPGA Specifications', 'clock_freq'))
+        self.bram = int(config.get('FPGA Specifications', 'bram'))
+        self.bram_bytes = int(config.get('FPGA Specifications', 'bram_type')) / 8
+        self.dsp = int(config.get('FPGA Specifications', 'dsp'))
+        self.mem_bw = float(config.get('FPGA Specifications', 'mem_bw'))
 
-    def get_dp_performance(self, rin, rout, muls, adds, mem):
+    def get_dp_performance(self, gamma, ii, muls, adds, mem, depth):
         mem_kb = (mem * self.word_bytes) / 1e3
         mem_bram = math.ceil(mem_kb / self.bram_bytes)
         bram_util = (mem_bram / self.bram) * 100
 
         dsps_util = (muls/self.dsp)*100
 
-        thr_in = self.cycles_per_sec * rin    # Words per second
+        rin = gamma[0,0]
+        rout = abs(gamma[-1,-1])
+        thr_in = self.cycles_per_sec * rin      # Words per second
         thr_out = self.cycles_per_sec * rout    # Words per second
 
-        return thr_in, thr_out, dsps_util, bram_util
+        latency = (depth + np.max(np.abs(ii)))/self.cycles_per_sec
+
+        return latency, thr_in, thr_out, dsps_util, bram_util
+
+    def balance_matrix(self, matrix):
+        rate_ratio = [abs(matrix[i,i]/matrix[i-1,i]) for i in range(1, matrix.shape[1]-1)]
+
+        for i in range(0, matrix.shape[0]):
+            layer = matrix.shape[0] - i
+            # print(layer, matrix[layer-1,layer], matrix[layer-1,layer-1])
+            if abs(matrix[layer-1,layer]) > matrix[layer-1,layer-1]:
+                # propogate forward
+                # print("balancing needed (forward pass)")
+                for j in range(layer,matrix.shape[0]+1):
+                    if abs(matrix[j-1,j]) <= matrix[j-1,j-1]:
+                        break
+                    # print(layer, matrix[j-1,j], matrix[j-1,j-1])
+                    matrix[j-1,j] = -matrix[j-1,j-1]
+                    if j < matrix.shape[0]:
+                        # print("ratio", j-1, rate_ratio[j-1])
+                        matrix[j,j] = matrix[j-1,j-1]*rate_ratio[j-1]
+                # print("finished balancing (forward pass)")
+            elif abs(matrix[layer-1,layer]) < matrix[layer-1,layer-1]:
+                # propogate backward
+                # print("balancing needed (backward pass)")
+                for j in range(0,layer):
+                    if(abs(matrix[layer-j-1,layer-j]) >= matrix[layer-j-1,layer-j-1]):
+                        break
+                    # print(layer, matrix[layer-j-1,layer-j], matrix[layer-j-1,layer-j-1])
+                    matrix[layer-j-1,layer-j-1]  = abs(matrix[layer-j-1,layer-j])
+                    if layer-j-1 > 0:
+                        # print("ratio", layer-1-j-1, rate_ratio[layer-1-j-1])
+                        matrix[layer-j-2,layer-j-1] = -matrix[layer-j-1,layer-j-1]/rate_ratio[layer-1-j-1]
+                # print("finished balancing (backward pass)")
+        
+        rate_ratio_new = [abs(matrix[i,i]/matrix[i-1,i]) for i in range(1, matrix.shape[1]-1)]
+        assert np.allclose(rate_ratio, rate_ratio_new), "{} - {}".format(rate_ratio, rate_ratio_new)
+
+        return matrix
+        
--- fpgaHART/layers/convolutional_3d.py
+++ fpgaHART/layers/convolutional_3d.py
@@ -1,9 +1,13 @@
 import numpy as np
 import math
 from .base_layer import BaseLayer
+np.set_printoptions(precision=5, suppress=True, linewidth=150)
 
+#TODO: *SOS* REVISE THE DEPTHWISE CONV!!!!! CAREFULL WITH THE CHANNEL DIVISION WITH GROUPS!!!! *SOS*
 class Convolutional3DLayer(BaseLayer):
     def __init__(self, description):
+        super().__init__()
+
         self.input_shape = description['shape_in'][0]
         self.depth_in = self.input_shape[2]
         self.rows_in = self.input_shape[3]
@@ -36,10 +40,17 @@ class Convolutional3DLayer(BaseLayer):
             self.depthwise = True
         elif np.prod(np.array(self.kernel_shape)) == 1:
             self.pointwise = True
-            
-    def get_design_point(self, f_fine, f_coarseIn, f_coarseOut):
-        
+
+    def update_layer(self):
+        self.channels = self.input_shape[1]
+
+    def get_design_point(self, f_fine, f_coarseIn, f_coarseOut, mem_bw_in, mem_bw_out):
+        self.update_layer()
+
         kernel_elems = int(np.prod(np.array(self.kernel_shape)))
+
+        if self.depthwise:
+            self.channels = self.channels//self.groups
 
         if self.pointwise:
             # Sliding Window Depth
@@ -70,19 +81,144 @@ class Convolutional3DLayer(BaseLayer):
         
         max_parallel_muls *= f_fine * f_coarseIn * f_coarseOut
         max_parallel_adds *= f_fine * f_coarseIn * f_coarseOut
-
-
-        rin_sw = 1                                                                                          # 1 * 
-        rout_sw = (self.depth_out*self.rows_out*self.cols_out)/(self.depth_in*self.rows_in*self.cols_in)    # Kd X Kw X Kh
-
-        rin_pop = 1
-        rout_pop = 1
         
-        rin_conv = f_fine           # Kd X Kw X Kh
-        rout_conv = f_fine          # 1
+        gamma_matrix = self.get_rate_matrix(f_fine) * self.get_stream_matrix(f_coarseIn, f_coarseOut) * self.get_data_matrix(mem_bw_in, mem_bw_out)
+        # print("Γ:\n{}".format(gamma_matrix))
+        gamma_matrix_balanced = self.balance_matrix(gamma_matrix)
+        # print("Γ Balanced:\n{}".format(gamma_matrix_balanced))
+        workload_matrix = self.get_workload_matrix(f_coarseIn, f_coarseOut)
+        # print("WorkLoad:\n{}".format(workload_matrix))
+        ii_matrix = np.nan_to_num(workload_matrix/gamma_matrix_balanced)
+        # print("II:\n{}".format(ii_matrix))
 
-        rin_accum = 1                   
-        rout_accum = 1/self.channels
+        latency, thr_in, thr_out, dsps_util, bram_util = self.get_dp_performance(gamma_matrix_balanced, ii_matrix, max_parallel_muls, max_parallel_adds, memory, depth)
 
-        thr_in, thr_out, dsps_util, bram_util = self.get_dp_performance(1, 1, max_parallel_muls, max_parallel_adds, memory)
-        print("DSP % = {}, BRAM % = {}".format(dsps_util, bram_util))
+        total_workload = self.depth_out*self.rows_out*self.cols_out*self.kd*self.kw*self.kh*self.channels*self.filters
+        throughput_gops = total_workload/latency
+        if not self.depthwise:
+            thr_in /= (self.depth_in * self.rows_in * self.cols_in * self.channels * f_coarseIn)        # Volumes per second
+        else:
+            thr_in /= (self.depth_in * self.rows_in * self.cols_in * self.channels)                     # Volumes per second
+        thr_out /= (self.depth_out * self.rows_out * self.cols_out * self.filters * f_coarseOut)        # Volumes per second
+        assert math.isclose(thr_in, thr_out), "Thoughputs missmatch. IN = {}, OUT = {}".format(thr_in, thr_out)
+
+        # print("DSP % = {:.2f}, BRAM % = {:.2f}, latency = {:.6f}, GOPs/s = {:.6f}, In Volumes/s = {:.6f}, Out Volumes/s = {:.6f}, depth = {}, workload(G) = {:.6f}".format(dsps_util, bram_util, latency, throughput_gops*1e-9, thr_in, thr_out, depth, total_workload*1e-9))
+
+    def get_rate_matrix(self, f_fine):
+        if not self.depthwise:
+            rate_matrix = np.zeros( shape=(5,6) , dtype=float )
+        else:
+            rate_matrix = np.zeros( shape=(4,5) , dtype=float )
+
+        # Memory bandwidth splitted between read and write
+        rate_matrix[0, 0] = 1
+        
+        rate_matrix[0, 1] = 1
+        rate_matrix[1, 1] = (self.depth_out*self.rows_out*self.cols_out)/(self.depth_in*self.rows_in*self.cols_in)
+
+        rate_matrix[1, 2] = 1
+        rate_matrix[2, 2] = 1
+
+        rate_matrix[2, 3] = f_fine
+        rate_matrix[3, 3] = f_fine
+
+        if not self.depthwise:
+            rate_matrix[3, 4] = 1
+            rate_matrix[4, 4] = 1
+
+            # Memory bandwidth splitted between read and write
+            rate_matrix[4, 5] = 1
+        else:
+            # Memory bandwidth splitted between read and write
+            rate_matrix[3, 4] = 1
+
+        # print("R:\n{}".format(rate_matrix))
+        return rate_matrix
+
+    def get_stream_matrix(self, f_coarseIn, f_coarseOut):
+        if not self.depthwise:
+            stream_matrix = np.zeros( shape=(5,6) , dtype=float )
+        else:
+            stream_matrix = np.zeros( shape=(4,5) , dtype=float )
+
+        stream_matrix[0, 0] = 1
+        
+        stream_matrix[0, 1] = self.channels * f_coarseIn
+        stream_matrix[1, 1] = self.channels * f_coarseIn
+
+        stream_matrix[1, 2] = self.channels * f_coarseIn
+        stream_matrix[2, 2] = self.channels * f_coarseIn * self.filters * f_coarseOut
+
+        stream_matrix[2, 3] = self.channels * f_coarseIn * self.filters * f_coarseOut
+        stream_matrix[3, 3] = self.channels * f_coarseIn * self.filters * f_coarseOut
+
+        if not self.depthwise:
+            stream_matrix[3, 4] = self.channels * f_coarseIn * self.filters * f_coarseOut
+            stream_matrix[4, 4] = self.filters * f_coarseOut
+
+            stream_matrix[4, 5] = 1
+        else:
+            stream_matrix[3, 4] = 1
+
+        # print("S:\n{}".format(stream_matrix))
+        return stream_matrix
+
+    def get_data_matrix(self, mem_bw_in, mem_bw_out):
+        if not self.depthwise:
+            data_matrix = np.zeros( shape=(5,6) , dtype=float )
+        else:
+            data_matrix = np.zeros( shape=(4,5) , dtype=float )
+
+        data_matrix[0, 0] = mem_bw_in
+        
+        data_matrix[0, 1] = -1
+        data_matrix[1, 1] = self.kd * self.kw * self.kh
+
+        data_matrix[1, 2] = -self.kd * self.kw * self.kh
+        data_matrix[2, 2] = self.kd * self.kw * self.kh
+
+        data_matrix[2, 3] = -self.kd * self.kw * self.kh
+        data_matrix[3, 3] = 1
+
+        if not self.depthwise:
+            data_matrix[3, 4] = -1
+            data_matrix[4, 4] = 1
+
+            data_matrix[4, 5] = -mem_bw_out
+        else:
+            data_matrix[3, 4] = -mem_bw_out
+
+        # print("D:\n{}".format(data_matrix))
+        return data_matrix
+
+    def get_workload_matrix(self, f_coarseIn, f_coarseOut):
+        in_volume = self.depth_in * self.rows_in * self.cols_in
+        out_volume = self.depth_out * self.rows_out * self.cols_out
+        kernel_volume = self.kd * self.kw * self.kh
+
+        if not self.depthwise:
+            workload_matrix = np.zeros( shape=(5,6) , dtype=float )
+        else:
+            workload_matrix = np.zeros( shape=(4,5) , dtype=float )
+
+        workload_matrix[0, 0] = in_volume * self.channels * f_coarseIn
+        
+        workload_matrix[0, 1] = in_volume * self.channels * f_coarseIn
+        workload_matrix[1, 1] = out_volume * kernel_volume * self.channels * f_coarseIn
+
+        workload_matrix[1, 2] = out_volume * kernel_volume * self.channels * f_coarseIn
+        workload_matrix[2, 2] = out_volume * kernel_volume * self.channels * self.filters * f_coarseIn * f_coarseOut
+
+        workload_matrix[2, 3] = out_volume * kernel_volume * self.channels * self.filters * f_coarseIn * f_coarseOut
+        workload_matrix[3, 3] = out_volume * self.channels * self.filters * f_coarseIn * f_coarseOut
+
+        if not self.depthwise:
+            workload_matrix[3, 4] = out_volume * self.channels * self.filters * f_coarseIn * f_coarseOut
+            workload_matrix[4, 4] = out_volume * self.filters * f_coarseOut
+
+            workload_matrix[4, 5] = out_volume * self.filters * f_coarseOut
+        else:
+            workload_matrix[3, 4] = out_volume * self.filters * f_coarseOut
+
+        # print("WL:\n{}".format(workload_matrix))
+        return workload_matrix
