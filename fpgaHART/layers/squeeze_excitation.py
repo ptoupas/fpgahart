dfcd35c45990bb3abadeefb66fb6d14820f5a48e
Petros Toupas <ptoupas@gmail.com>
2021-10-14 04:01:48 +0300

    more work on se layer and added multithreaded execution for brute force search

--- fpgaHART/layers/squeeze_excitation.py
+++ fpgaHART/layers/squeeze_excitation.py
@@ -4,7 +4,9 @@ import scipy.optimize as optimize
 from scipy.optimize import NonlinearConstraint, Bounds
 from .base_layer import BaseLayer
 from .convolutional_3d import Convolutional3DLayer
+from .gap import GAPLayer
 np.set_printoptions(precision=5, suppress=True, linewidth=150)
+np.seterr(divide='ignore', invalid='ignore')
 
 DEBUG=False
 
@@ -13,32 +15,40 @@ class SqueezeExcitationLayer(BaseLayer):
         super().__init__()
         self.optimization = optimization
 
+        self.branching = description['branching']
+        if self.branching:
+            self.branch_shape = description['shape_branch']
+        self.input_shape = description['shape_in'][0]
+        self.output_shape = description['shape_out']
+
         self.sequencial = {}
         for n_se, l_se in description['primitive_ops'].items():
-          if l_se['operation'] == 'GlobalAveragePool':
-            continue
-            self.sequencial[n_se] = GAPLayer(l_se, optimization)
-          elif l_se['operation'] == 'Conv':
-            self.sequencial[n_se] = Convolutional3DLayer(l_se, optimization)
-          elif l_se['operation'] == 'Relu' or l_se['operation'] == 'Sigmoid':
-            continue
-            self.sequencial[n_se] = ActivationLayer(l_se, optimization)
-          elif l_se['operation'] == 'Mul':
-            continue
-            self.sequencial[n_se] = ElementWiseLayer(l_se, optimization)
-        
+            if l_se['operation'] == 'GlobalAveragePool':
+                self.sequencial[n_se] = GAPLayer(l_se, optimization)
+            elif l_se['operation'] == 'Conv':
+                self.sequencial[n_se] = Convolutional3DLayer(l_se, optimization)
+            elif l_se['operation'] == 'Relu' or l_se['operation'] == 'Sigmoid':
+                continue
+                self.sequencial[n_se] = ActivationLayer(l_se, optimization)
+            elif l_se['operation'] == 'Mul':
+                continue
+                self.sequencial[n_se] = ElementWiseLayer(l_se, optimization)
         self.num_layers = len(self.sequencial) + 2
-        self.gamma_matrix = np.zeros( shape=(self.num_layers-1, self.num_layers) , dtype=float )
-        for l in self.sequencial.keys():
-          print(l, self.sequencial[l].input_shape, self.sequencial[l].output_shape, self.sequencial[l].kernel_shape)
 
-    def get_design_point(self, f_fine_1, f_coarseIn_1, f_coarseOut_1, f_fine_2, f_coarseIn_2, f_coarseOut_2, mem_bw_in, mem_bw_out):
-        self.gamma_matrix[0, 0] = mem_bw_in
-        self.gamma_matrix[-1, -1] = -mem_bw_out
+    def get_latency(self, params):
+        f_fine_1, f_coarseIn_1, f_coarseOut_1, f_fine_2, f_coarseIn_2, f_coarseOut_2, mem_bw_in, mem_bw_out = params
+        if not (f_fine_1>0 and f_coarseIn_1>0 and f_coarseOut_1>0 and f_fine_2>0 and f_coarseIn_2>0 and f_coarseOut_2>0 and mem_bw_in>0 and mem_bw_out>0):
+            return -1
+
+        gamma_matrix = np.zeros( shape=(self.num_layers-1, self.num_layers) , dtype=float )
+        gamma_matrix[0, 0] = mem_bw_in
+        gamma_matrix[-1, -1] = -mem_bw_out
 
         total_muls = 0
+        total_adds = 0
         total_memory = 0
         total_depth = 0
+        first_layer_bw_in = False
         #TODO: Find a way to discriminate between different layers to pass them the correct configuration
         prev_layer_rate = mem_bw_in
         for n, l in enumerate(self.sequencial.keys()):
@@ -46,17 +56,137 @@ class SqueezeExcitationLayer(BaseLayer):
                 curr_layer_rate = mem_bw_out
             else:
                 curr_layer_rate = 10000000
-            print("A", prev_layer_rate, curr_layer_rate)
-            self.sequencial[l].get_design_point(f_fine_1, f_coarseIn_1, f_coarseOut_1, prev_layer_rate, curr_layer_rate)
-            full_rate_in, full_rate_out, muls, memory, depth = self.sequencial[l].get_dp_info()
+
+            if n==0:
+                self.sequencial[l].get_design_point(f_gap_coarseinout, prev_layer_rate, curr_layer_rate)
+            elif n==1:
+                self.sequencial[l].get_design_point(f_fine_1, f_coarseIn_1, f_coarseOut_1, prev_layer_rate, curr_layer_rate)
+            elif n==2:
+                self.sequencial[l].get_design_point(f_fine_2, f_coarseIn_2, f_coarseOut_2, prev_layer_rate, curr_layer_rate)
+            if n==0:
+               first_layer_bw_in = mem_bd_in
+            full_rate_in, full_rate_out, muls, adds, memory, depth, mem_bd_in, mem_bd_out = self.sequencial[l].get_dp_info()
             prev_layer_rate = full_rate_out
-            print(full_rate_in, full_rate_out, muls, memory, depth)
-            self.gamma_matrix[n, n+1] = full_rate_in
-            self.gamma_matrix[n+1, n+1] = full_rate_out
+            gamma_matrix[n, n+1] = -full_rate_in
+            gamma_matrix[n+1, n+1] = full_rate_out
             total_muls += muls
+            total_adds += adds
             total_memory += memory
             total_depth += depth
 
-        gamma_matrix_balanced, mem_bounded_in, mem_bounded_out = self.balance_matrix(self.gamma_matrix.copy())
-        print(gamma_matrix_balanced)
-        print("Bounded in = {}, Bounded out = {}".format(mem_bounded_in, mem_bounded_out))
+        gamma_matrix_balanced, mem_bounded_in, mem_bounded_out = self.balance_matrix(gamma_matrix.copy())
+        if DEBUG:
+            print("Γ:\n{}".format(gamma_matrix_balanced))
+        workload_matrix = self.get_workload_matrix()
+        if DEBUG:
+            print("WL:\n{}".format(workload_matrix))
+        ii_matrix = np.nan_to_num(workload_matrix/gamma_matrix_balanced)
+        if DEBUG:
+            print("II:\n{}".format(ii_matrix))
+
+        mem_bounded_in = mem_bounded_in or first_layer_bw_in
+        latency_sec, latency_cycles, thr_in, thr_out, dsps_util, bram_util = self.get_dp_performance(workload_matrix, ii_matrix, total_muls, total_adds, total_memory, total_depth)
+
+        latency_cycles = np.max(np.abs(ii_matrix)) + total_depth
+        penalize_factor = (bram_util/100 + dsps_util/100) * (np.max(np.abs(ii_matrix)) + total_depth)
+
+        if self.optimization == 'Powell':
+            optimization_score = latency_cycles + penalize_factor
+        elif self.optimization == 'trust-constr':
+            optimization_score = latency_cycles
+        
+        return optimization_score
+
+    def get_design_point(self, f_gap_coarseinout, f_fine_1, f_coarseIn_1, f_coarseOut_1, f_fine_2, f_coarseIn_2, f_coarseOut_2, mem_bw_in, mem_bw_out):
+
+        if self.optimization == 'Powell':
+            initial_guess = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, mem_bw_in, mem_bw_out]
+            bnds = ((0.01, 1.0), (0.01, 1.0), (0.01, 1.0), (0.01, 1.0), (0.01, 1.0), (0.01, 1.0), (0.01, 1.0), (0.01, mem_bw_in), (0.01, mem_bw_out))   
+            result = optimize.minimize(self.get_latency, initial_guess, method=self.optimization, bounds=bnds)
+            print(result)
+            if result.success:
+                f_gap_coarseinout, f_fine_1, f_coarseIn_1, f_coarseOut_1, f_fine_2, f_coarseIn_2, f_coarseOut_2, mem_bw_in, mem_bw_out = result.x
+            else:
+                print("Failed to optimize. Skipping...")
+                return
+
+        gamma_matrix = np.zeros( shape=(self.num_layers-1, self.num_layers) , dtype=float )
+        gamma_matrix[0, 0] = mem_bw_in
+        gamma_matrix[-1, -1] = -mem_bw_out
+
+        total_muls = 0
+        total_adds = 0
+        total_memory = 0
+        total_depth = 0
+        first_layer_bw_in = False
+        #TODO: Find a way to discriminate between different layers to pass them the correct configuration
+        prev_layer_rate = mem_bw_in
+        for n, l in enumerate(self.sequencial.keys()):
+            if n == len(self.sequencial) - 1:
+                curr_layer_rate = mem_bw_out
+            else:
+                curr_layer_rate = 10000000
+            
+            if n==0:
+                self.sequencial[l].get_design_point(f_gap_coarseinout, prev_layer_rate, curr_layer_rate)
+            elif n==1:
+                self.sequencial[l].get_design_point(f_fine_1, f_coarseIn_1, f_coarseOut_1, prev_layer_rate, curr_layer_rate)
+            elif n==2:
+                self.sequencial[l].get_design_point(f_fine_2, f_coarseIn_2, f_coarseOut_2, prev_layer_rate, curr_layer_rate)
+            full_rate_in, full_rate_out, muls, adds, memory, depth, mem_bd_in, mem_bd_out = self.sequencial[l].get_dp_info()
+            if n==0:
+               first_layer_bw_in = mem_bd_in
+            prev_layer_rate = full_rate_out
+            gamma_matrix[n, n+1] = -full_rate_in
+            gamma_matrix[n+1, n+1] = full_rate_out
+            total_muls += muls
+            total_adds += adds
+            total_memory += memory
+            total_depth += depth
+
+        if DEBUG:
+            print("Γ:\n{}".format(gamma_matrix))
+        gamma_matrix_balanced, mem_bounded_in, mem_bounded_out = self.balance_matrix(gamma_matrix.copy())
+        if DEBUG:
+            print("Γ Balanced:\n{}".format(gamma_matrix_balanced))
+        workload_matrix = self.get_workload_matrix()
+        if DEBUG:
+            print("WL:\n{}".format(workload_matrix))
+        ii_matrix = np.nan_to_num(workload_matrix/gamma_matrix_balanced)
+        if DEBUG:
+            print("II:\n{}".format(ii_matrix))
+
+        mem_bounded_in = mem_bounded_in or first_layer_bw_in
+        latency_sec, latency_cycles, thr_in, thr_out, dsps_util, bram_util = self.get_dp_performance(workload_matrix, ii_matrix, total_muls, total_adds, total_memory, total_depth)
+        total_ops = self.get_total_workload()
+        throughput_ops = total_ops/latency_sec
+        thr_in /= workload_matrix[0,0]              # Volumes per second
+        thr_out /= workload_matrix[-1,-1]           # Volumes per second
+        assert math.isclose(thr_in, thr_out), "Thoughputs missmatch. IN = {}, OUT = {}.".format(thr_in, thr_out)
+
+        if DEBUG:
+            if dsps_util < 90. and bram_util < 90.:
+                print("GOPs/s={:.2f}, DSPS={:.2f}, BRAM={:.2f}, latency(s)={:.2f}, latency(c)={:.2f}, mem bounded in = {}, mem bounded out = {}".format(throughput_ops*1e-9, dsps_util, bram_util, latency_sec, latency_cycles, mem_bounded_in, mem_bounded_out))
+            else:
+                print("Discarding design point.")
+
+        return f_gap_coarseinout, f_fine_1, f_coarseIn_1, f_coarseOut_1, f_fine_2, f_coarseIn_2, f_coarseOut_2, mem_bw_in, mem_bw_out, dsps_util, bram_util, latency_sec, int(latency_cycles), throughput_ops, thr_in, thr_out, depth, total_ops, mem_bounded_in, mem_bounded_out
+        
+    def get_total_workload(self):
+        total_wl = 0
+        for n, l in enumerate(self.sequencial.keys()):
+            total_wl += self.sequencial[l].get_total_workload()
+
+        return total_wl
+
+    def get_workload_matrix(self):
+        workload_matrix = np.zeros( shape=(self.num_layers-1, self.num_layers) , dtype=float )
+
+        for n, l in enumerate(self.sequencial.keys()):
+            if n == 0:
+                workload_matrix[0, 0] = np.prod(np.array(self.sequencial[l].input_shape[1:]))
+            if n == len(self.sequencial) - 1:
+                workload_matrix[-1, -1] = np.prod(np.array(self.sequencial[l].output_shape[1:]))
+            workload_matrix[n, n+1] = np.prod(np.array(self.sequencial[l].input_shape[1:]))
+            workload_matrix[n+1, n+1] = np.prod(np.array(self.sequencial[l].output_shape[1:]))
+        return workload_matrix
--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import argparse
 from fpgaHART.onnx_parser.partition_descriptor import PartitionDescriptor
 from fpgaHART.layers.convolutional_3d import Convolutional3DLayer
 from fpgaHART.layers.squeeze_excitation import SqueezeExcitationLayer
+from fpgaHART.layers.gap import GAPLayer
 import itertools
+import time
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
+from multiprocessing import Pool
 
 sns.set(rc={'figure.figsize':(15,8)})
 sns.set_style("whitegrid")
@@ -17,9 +20,14 @@ def parse_args():
     parser = argparse.ArgumentParser(description='fpgaHART toolflow parser')
     parser.add_argument('model_name', help='name of the HAR model')
     parser.add_argument('--optimization', type=str, default='brute_force', choices=['brute_force', 'Powell', 'trust-constr'], help='Optimization Strategy')
+    parser.add_argument('--singlethreaded', action='store_true', help='whether to use single thread solution or not')
 
     args = parser.parse_args()
     return args
+
+def multithreaded_modeling(operation, input, pool):
+    results = pool.starmap(operation, input)
+    return results
 
 if __name__ == '__main__':
     args = parse_args()
@@ -28,6 +36,7 @@ if __name__ == '__main__':
     # ModelLayerDescriptor(args.model_name)
     partitions = PartitionDescriptor(args.model_name)
 
+    start = time.time()
     for n,l in partitions.layers.items():
         if l['operation'] == 'Conv':
             conv = Convolutional3DLayer(l, args.optimization)
@@ -54,41 +63,77 @@ if __name__ == '__main__':
             
             max_throughput = -1
             best = []
-            for (f, c1, c2, (bw_in, bw_out)) in combinations:
-                f_fine, f_fine_elems, f_coarseIn, f_coarseIn_ch, f_coarseOut, f_coarseOut_fil, mem_bw_in, mem_bw_out, dsps_util, max_parallel_muls, bram_util, latency_sec, latency_cycles, throughput_gops, thr_in, thr_out, depth, total_ops, mem_bounded_in, mem_bounded_out = conv.get_design_point(f, c1, c2, conv.mem_words_per_cycle*bw_in, conv.mem_words_per_cycle*bw_out)
-                if throughput_gops > max_throughput and (dsps_util < 90. and bram_util < 90.):
-                    max_throughput = throughput_gops
-                    best = [f_fine, f_fine_elems, f_coarseIn, f_coarseIn_ch, f_coarseOut, f_coarseOut_fil, mem_bw_in, mem_bw_out, dsps_util, max_parallel_muls, bram_util, latency_sec, latency_cycles, throughput_gops, thr_in, thr_out, depth, total_ops, mem_bounded_in, mem_bounded_out]
-        
-            print("(fine={:.2f}({}), cIn={:.2f}({}), cOut={:.2f}({}), bwIn={:.2f}, bwOut={:.2f}) DSP % = {:.2f} ({}), BRAM % = {:.2f}, latency = {:.5f}({}), GOPs/s = {:.5f}, In Volumes/s = {:.5f}, Out Volumes/s = {:.5f}, depth = {}, workload(G) = {:.5f}, Mem Bound In={}, Mem Bound Out={}".format(best[0], best[1], best[2], best[3], best[4], best[5], best[6], best[7], best[8], best[9], best[10], best[11], best[12], best[13], best[14], best[15], best[16], best[17], best[18], best[19]))
+            if not args.singlethreaded:
+                processes_pool = Pool(10)
+                input_vars = []
+                for (f, c1, c2, (bw_in, bw_out)) in combinations:
+                    input_vars.append([f, c1, c2, conv.mem_words_per_cycle*bw_in, conv.mem_words_per_cycle*bw_out])
+                results = multithreaded_modeling(conv.get_design_point, input_vars, processes_pool)
+                for r in results:
+                    if r[13] > max_throughput and (r[8] < 90. and r[10] < 90.):
+                        max_throughput = r[13]
+                        best = list(r)
+            else:
+                for (f, c1, c2, (bw_in, bw_out)) in combinations:
+                    params = conv.get_design_point(f, c1, c2, conv.mem_words_per_cycle*bw_in, conv.mem_words_per_cycle*bw_out)
+                    if params[13] > max_throughput and (params[8] < 90. and params[10] < 90.):
+                        max_throughput = params[13]
+                        best = list(params)   
+
+            print("(fine={:.2f}({}), cIn={:.2f}({}), cOut={:.2f}({}), bwIn={:.2f}, bwOut={:.2f}) DSP % = {:.2f} ({}), BRAM % = {:.2f}, latency = {:.5f}({}), GOPs/s = {:.5f}, In Volumes/s = {:.5f}, Out Volumes/s = {:.5f}, depth = {}, workload(G) = {:.5f}, Mem Bound In={}, Mem Bound Out={}".format(best[0], best[1], best[2], best[3], best[4], best[5], best[6], best[7], best[8], best[9], best[10], best[11], best[12], best[13]*1e-9, best[14], best[15], best[16], best[17]*1e-9, best[18], best[19]))
             print("*"*40)
         elif l['operation'] == 'SqueezeExcitation':
             se = SqueezeExcitationLayer(l, args.optimization)
+            print("{}. In shape {}, Out shape {}".format(n, se.input_shape, se.output_shape))
 
+            layer_channels = []
+            layer_filters = []
+            for layers in se.sequencial.values():
+                layer_channels.append(layers.channels)
+                layer_filters.append(layers.filters)
             if args.optimization == 'brute_force':
-                fine_1 = [1]
-                coarsein_1 = [1/conv.channels, 0.2, 0.4, 0.5, 0.6, 0.8, 1]
-                coarseout_1 = [1/conv.filters, 0.1, 0.3, 0.5, 0.7, 0.9, 0.1]
-                fine_2 = [1]
-                coarsein_2 = [1/conv.channels, 0.2, 0.4, 0.5, 0.6, 0.8, 1]
-                coarseout_2 = [1/conv.filters, 0.1, 0.3, 0.5, 0.7, 0.9, 0.1]
+                gap_coarseinout = [1/layer_channels[0], 0.2, 0.4, 0.5, 0.6, 0.8, 1]
+                coarsein_1 = [1/layer_channels[1], 0.2, 0.4, 0.5, 0.6, 0.8, 1]
+                coarseout_1 = [1/layer_filters[1], 0.1, 0.3, 0.5, 0.7, 0.9, 0.1]
+                coarsein_2 = [1/layer_channels[2], 0.2, 0.4, 0.5, 0.6, 0.8, 1]
+                coarseout_2 = [1/layer_filters[2], 0.1, 0.3, 0.5, 0.7, 0.9, 0.1]
             else:
-                fine_1 = [1]
+                gap_coarseinout = [1]
                 coarsein_1 = [1]
                 coarseout_1 = [1]
-                fine_2 = [1]
                 coarsein_2 = [1]
                 coarseout_2 = [1]
+            fine_1 = [1]
+            fine_2 = [1]
             mem_bw = [(0.1,0.9), (0.2,0.8), (0.3,0.7), (0.4,0.6), (0.5,0.5), (0.6,0.4), (0.7,0.3), (0.8,0.2), (0.9,0.1)]
-            total = [fine_1, coarsein_1, coarseout_1, fine_2, coarsein_2, coarseout_2, mem_bw]
+            
+            total = [gap_coarseinout, fine_1, coarsein_1, coarseout_1, fine_2, coarsein_2, coarseout_2, mem_bw]
             combinations = itertools.product(*total)
 
-            for (f1, c11, c21, f2, c12, c22, (bw_in, bw_out)) in combinations:
-                se.get_design_point(f1, c11, c21, f2, c12, c22, bw_in, bw_out)
-                exit()
-            exit()
+            max_throughput = -1
+            best = []
+            if not args.singlethreaded:
+                processes_pool = Pool(10)
+                input_vars = []
+                for (cinout, f1, c11, c21, f2, c12, c22, (bw_in, bw_out)) in combinations:
+                    input_vars.append([cinout, f1, c11, c21, f2, c12, c22, se.mem_words_per_cycle*bw_in, se.mem_words_per_cycle*bw_out])
+                results = multithreaded_modeling(se.get_design_point, input_vars, processes_pool)
+                for r in results:
+                    if r[13] > max_throughput and (r[9] < 90. and r[10] < 90.):
+                        max_throughput = r[13]
+                        best = list(r)
+            else:
+                for (cinout, f1, c11, c21, f2, c12, c22, (bw_in, bw_out)) in combinations:
+                    params = se.get_design_point(cinout, f1, c11, c21, f2, c12, c22, se.mem_words_per_cycle*bw_in, se.mem_words_per_cycle*bw_out)
+                    if params[13] > max_throughput and (params[9] < 90. and params[10] < 90.):
+                        max_throughput = params[13]
+                        best = list(params)
+            print("(cinout = {:.2f}, fine1={:.2f}, coarsein1={:.2f}, coarseout1={:.2f}, fine2={:.2f}, coarsein2={:.2f}, coarseout2={:.2f}, bwIn={:.2f}, bwOut={:.2f}) DSP % = {:.2f}, BRAM % = {:.2f}, latency = {:.5f}({}), GOPs/s = {:.5f}, In Volumes/s = {:.5f}, Out Volumes/s = {:.5f}, depth = {}, workload(G) = {:.5f}, Mem Bound In={}, Mem Bound Out={}".format(best[0], best[1], best[2], best[3], best[4], best[5], best[6], best[7], best[8], best[9], best[10], best[11], best[12], best[13]*1e-9, best[14], best[15], best[16], best[17]*1e-9, best[18], best[19]))
+            print("*"*40)
+
             for n_se,l_se in l['primitive_ops'].items():
                 if l_se['operation'] == 'Conv':
+                    continue
                     conv = Convolutional3DLayer(l_se, args.optimization)
                     if conv.depthwise:
                         convtype = 'DepthWise'
@@ -114,10 +159,77 @@ if __name__ == '__main__':
                     max_throughput = -1
                     best = []
                     for (f, c1, c2, (bw_in, bw_out)) in combinations:
-                        f_fine, f_fine_elems, f_coarseIn, f_coarseIn_ch, f_coarseOut, f_coarseOut_fil, mem_bw_in, mem_bw_out, dsps_util, max_parallel_muls, bram_util, latency_sec, latency_cycles, throughput_gops, thr_in, thr_out, depth, total_ops, mem_bounded_in, mem_bounded_out = conv.get_design_point(f, c1, c2, conv.mem_words_per_cycle*bw_in, conv.mem_words_per_cycle*bw_out)
-                        if throughput_gops > max_throughput and (dsps_util < 90. and bram_util < 90.):
-                            max_throughput = throughput_gops
-                            best = [f_fine, f_fine_elems, f_coarseIn, f_coarseIn_ch, f_coarseOut, f_coarseOut_fil, mem_bw_in, mem_bw_out, dsps_util, max_parallel_muls, bram_util, latency_sec, latency_cycles, throughput_gops, thr_in, thr_out, depth, total_ops, mem_bounded_in, mem_bounded_out]
-
-                    print("(fine={:.2f}({}), cIn={:.2f}({}), cOut={:.2f}({}), bwIn={:.2f}, bwOut={:.2f}) DSP % = {:.2f} ({}), BRAM % = {:.2f}, latency = {:.5f}({}), GOPs/s = {:.5f}, In Volumes/s = {:.5f}, Out Volumes/s = {:.5f}, depth = {}, workload(G) = {:.5f}, Mem Bound In={}, Mem Bound Out={}".format(best[0], best[1], best[2], best[3], best[4], best[5], best[6], best[7], best[8], best[9], best[10], best[11], best[12], best[13], best[14], best[15], best[16], best[17], best[18], best[19]))
+                        f_fine, f_fine_elems, f_coarseIn, f_coarseIn_ch, f_coarseOut, f_coarseOut_fil, mem_bw_in, mem_bw_out, dsps_util, max_parallel_muls, bram_util, latency_sec, latency_cycles, throughput_ops, thr_in, thr_out, depth, total_ops, mem_bounded_in, mem_bounded_out = conv.get_design_point(f, c1, c2, conv.mem_words_per_cycle*bw_in, conv.mem_words_per_cycle*bw_out)
+                        if throughput_ops > max_throughput and (dsps_util < 90. and bram_util < 90.):
+                            max_throughput = throughput_ops
+                            best = [f_fine, f_fine_elems, f_coarseIn, f_coarseIn_ch, f_coarseOut, f_coarseOut_fil, mem_bw_in, mem_bw_out, dsps_util, max_parallel_muls, bram_util, latency_sec, latency_cycles, throughput_ops, thr_in, thr_out, depth, total_ops, mem_bounded_in, mem_bounded_out]
+                    
+                    print("(fine={:.2f}({}), cIn={:.2f}({}), cOut={:.2f}({}), bwIn={:.2f}, bwOut={:.2f}) DSP % = {:.2f} ({}), BRAM % = {:.2f}, latency = {:.5f}({}), GOPs/s = {:.5f}, In Volumes/s = {:.5f}, Out Volumes/s = {:.5f}, depth = {}, workload(G) = {:.5f}, Mem Bound In={}, Mem Bound Out={}".format(best[0], best[1], best[2], best[3], best[4], best[5], best[6], best[7], best[8], best[9], best[10], best[11], best[12], best[13]*1e-9, best[14], best[15], best[16], best[17]*1e-9, best[18], best[19]))
                     print("*"*40)
+                elif l_se['operation'] == 'GlobalAveragePool':
+                    continue
+                    gap = GAPLayer(l_se, args.optimization)
+
+                    print("*"*40)
+                    print("{}. In shape {}, Out shape {}".format(n_se, gap.input_shape, gap.output_shape))
+                    coarseinout = [1/gap.channels, 0.2, 0.4, 0.5, 0.6, 0.8, 1]
+                    mem_bw = [(0.1,0.9), (0.2,0.8), (0.3,0.7), (0.4,0.6), (0.5,0.5), (0.6,0.4), (0.7,0.3), (0.8,0.2), (0.9,0.1)]
+                    total = [coarseinout, mem_bw]
+                    combinations = itertools.product(*total)
+                    
+                    min_latency = 10000000000
+                    best = []
+                    if not args.singlethreaded:
+                        processes_pool = Pool(10)
+                        input_vars = []
+                        for (cinout, (bw_in, bw_out)) in combinations:
+                            input_vars.append([cinout, gap.mem_words_per_cycle*bw_in, gap.mem_words_per_cycle*bw_out])
+                        results = multithreaded_modeling(gap.get_design_point, input_vars, processes_pool)
+                        for r in results:
+                            if r[7] < min_latency and (r[3] < 90. and r[5] < 90.):
+                                min_latency = r[7]
+                                best = list(r)
+                    else:
+                        for (cinout, (bw_in, bw_out)) in combinations:
+                            params = gap.get_design_point(cinout, gap.mem_words_per_cycle*bw_in, gap.mem_words_per_cycle*bw_out)
+                            if params[7] < min_latency and (params[3] < 90. and params[5] < 90.):
+                                min_latency = params[7]
+                                best = list(params)
+
+                    print("(coarseinout={:.2f}, bwIn={:.2f}, bwOut={:.2f}) DSP % = {:.2f}, BRAM % = {:.2f}, latency = {:.5f}({}), GOPs/s = {:.5f}, In Volumes/s = {:.5f}, Out Volumes/s = {:.5f}, depth = {}, workload(G) = {:.5f}, Mem Bound In={}, Mem Bound Out={}".format(best[0], best[1], best[2], best[3], best[5], best[6], best[7], best[8]*1e-9, best[9], best[10], best[11], best[12]*1e-9, best[13], best[14]))
+                    print("*"*40)
+        elif l['operation'] == 'GlobalAveragePool':
+            continue
+            gap = GAPLayer(l, args.optimization)
+
+            print("*"*40)
+            print("{}. In shape {}, Out shape {}".format(n, gap.input_shape, gap.output_shape))
+            coarseinout = [1/gap.channels, 0.2, 0.4, 0.5, 0.6, 0.8, 1]
+            mem_bw = [(0.1,0.9), (0.2,0.8), (0.3,0.7), (0.4,0.6), (0.5,0.5), (0.6,0.4), (0.7,0.3), (0.8,0.2), (0.9,0.1)]
+            total = [coarseinout, mem_bw]
+            combinations = itertools.product(*total)
+            
+            min_latency = 10000000000
+            best = []
+            if not args.singlethreaded:
+                processes_pool = Pool(10)
+                input_vars = []
+                for (cinout, (bw_in, bw_out)) in combinations:
+                    input_vars.append([cinout, gap.mem_words_per_cycle*bw_in, gap.mem_words_per_cycle*bw_out])
+                results = multithreaded_modeling(gap.get_design_point, input_vars, processes_pool)
+                for r in results:
+                    if r[7] < min_latency and (r[3] < 90. and r[5] < 90.):
+                        min_latency = r[7]
+                        best = list(r)
+            else:
+                for (cinout, (bw_in, bw_out)) in combinations:
+                    params = gap.get_design_point(cinout, gap.mem_words_per_cycle*bw_in, gap.mem_words_per_cycle*bw_out)
+                    if params[7] < min_latency and (params[3] < 90. and params[5] < 90.):
+                        min_latency = params[7]
+                        best = list(params)
+
+            print("(coarseinout={:.2f}, bwIn={:.2f}, bwOut={:.2f}) DSP % = {:.2f}, BRAM % = {:.2f}, latency = {:.5f}({}), GOPs/s = {:.5f}, In Volumes/s = {:.5f}, Out Volumes/s = {:.5f}, depth = {}, workload(G) = {:.5f}, Mem Bound In={}, Mem Bound Out={}".format(best[0], best[1], best[2], best[3], best[5], best[6], best[7], best[8]*1e-9, best[9], best[10], best[11], best[12]*1e-9, best[13], best[14]))
+            print("*"*40)
+            
+    end = time.time()
+    print("Execution time: {:.3f}".format(end-start))
