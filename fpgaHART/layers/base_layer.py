multiple fixes around the code (fix on graph balancing for broadcasting behaviour)

fpgaHART/layers/base_layer.py
@@ -32,7 +32,7 @@ class BaseLayer():
 
         dsps_util = (muls/self.dsp)*100
 
-        latency_cycles = np.max(np.abs(ii)) + depth
+        latency_cycles = np.max(np.abs(ii)) # + depth
         latency_sec = latency_cycles/self.cycles_per_sec
 
         thr_in = workload_matrix[0,0]/latency_sec       # Input words per second
@@ -166,22 +166,28 @@ class BaseLayer():
 
         return matrix, mem_bounded_in_1, mem_bounded_in_2, mem_bounded_out
 
-    def balance_matrix_elemwise_broadcasting(self, matrix, branch_node):
+    def balance_matrix_elemwise_broadcasting(self, matrix, branch_node, branch_ratio):
         mem_bounded_in_1 = False
         mem_bounded_in_2 = False
         mem_bounded_out = False
-
-        branch_ratio = abs(matrix[0,branch_node])/abs(matrix[branch_node-1,branch_node])
+        
+        curr_branch_ratio = 1/(abs(matrix[0, branch_node])/abs(matrix[branch_node-1, branch_node]))
+        if curr_branch_ratio < branch_ratio:
+            matrix[0, branch_node] = -abs(matrix[branch_node-1, branch_node]) / branch_ratio
+        elif curr_branch_ratio > branch_ratio:
+            matrix[branch_node-1, branch_node] = -abs(matrix[0, branch_node]) * branch_ratio
 
         if abs(matrix[branch_node-1, branch_node]) > matrix[branch_node-1, branch_node-1]:
             mem_bounded_in_2 = True
             matrix[branch_node-1, branch_node] = -matrix[branch_node-1, branch_node-1]
+            matrix[0, branch_node] = -abs(matrix[branch_node-1, branch_node]) / branch_ratio
         else:
             matrix[branch_node-1, branch_node-1] = abs(matrix[branch_node-1, branch_node])
 
         if abs(matrix[0, branch_node]) > matrix[0, 0]:
             mem_bounded_in_1 = True
             matrix[0, branch_node] = -matrix[0,0]
+            matrix[branch_node-1, branch_node] = -abs(matrix[0, branch_node]) * branch_ratio
         else:
             matrix[0,0] = abs(matrix[0, branch_node])
 
@@ -193,21 +199,17 @@ class BaseLayer():
         else:
             matrix[-1, -1] = -matrix[branch_node,branch_node]
 
-        if branch_ratio >= abs(matrix[0,branch_node])/abs(matrix[branch_node-1,branch_node]):
-            pass
-        else:
-            matrix[branch_node,branch_node] = branch_ratio*abs(matrix[branch_node-1,branch_node])
-            matrix[0,branch_node] = branch_ratio*abs(matrix[branch_node-1,branch_node])
-            matrix[0,0] = branch_ratio*abs(matrix[branch_node-1,branch_node])
-            matrix[-1,-1] = branch_ratio*abs(matrix[branch_node-1,branch_node])
-            branch_ratio = abs(matrix[0,branch_node])/abs(matrix[branch_node-1,branch_node])
+        curr_branch_ratio = 1/(abs(matrix[branch_node, branch_node])/abs(matrix[branch_node-1, branch_node]))
+        if curr_branch_ratio > branch_ratio:
+            matrix[branch_node-1, branch_node] = -abs(matrix[branch_node, branch_node]) * branch_ratio
+            matrix[branch_node-1, branch_node-1] = abs(matrix[branch_node, branch_node]) * branch_ratio
 
-        if matrix[branch_node,branch_node] <= abs(matrix[0,branch_node]):
             matrix[0,branch_node] = -matrix[branch_node,branch_node]
             matrix[0,0] = matrix[branch_node,branch_node]
-        else:
-            assert False, "Failed to move backwards on Γ matrix for input 1"
         
-        assert branch_ratio >= abs(matrix[0,branch_node])/abs(matrix[branch_node-1,branch_node]), "Problem with the graph balancing"
+        curr_branch_ratio = 1/(abs(matrix[0, branch_node])/abs(matrix[branch_node-1, branch_node]))
+        assert branch_ratio == curr_branch_ratio, "Problem with the graph balancing"
+        curr_branch_ratio = 1/(abs(matrix[branch_node, branch_node])/abs(matrix[branch_node-1, branch_node]))
+        assert branch_ratio == curr_branch_ratio, "Problem with the graph balancing"
 
         return matrix, mem_bounded_in_1, mem_bounded_in_2, mem_bounded_out

fpgaHART/primitive_blocks/elemwise.py
@@ -112,7 +112,9 @@ class ElementWiseLayer(BaseLayer):
         if DEBUG:
             print("Γ:\n{}".format(gamma_matrix))
         if self.broadcasting:
-            gamma_matrix_balanced, mem_bounded_in_1, mem_bounded_in_2, mem_bounded_out = self.balance_matrix_elemwise_broadcasting(gamma_matrix.copy(), 2)
+            broadcasted_shape = self.input_shape_1 if int(np.prod(np.array(self.input_shape_1[1:]))) > int(np.prod(np.array(self.input_shape_2[1:]))) else self.input_shape_2
+            branch_ratio = 1/(int(np.prod(np.array(broadcasted_shape[2:]))))
+            gamma_matrix_balanced, mem_bounded_in_1, mem_bounded_in_2, mem_bounded_out = self.balance_matrix_elemwise_broadcasting(gamma_matrix.copy(), 2, branch_ratio)
         else:
             gamma_matrix_balanced, mem_bounded_in_1, mem_bounded_in_2, mem_bounded_out = self.balance_matrix_elemwise(gamma_matrix.copy(), 2)
         if DEBUG:
@@ -208,7 +210,9 @@ class ElementWiseLayer(BaseLayer):
         if DEBUG:
             print("Γ:\n{}".format(gamma_matrix))
         if self.broadcasting:
-            gamma_matrix_balanced, mem_bounded_in_1, mem_bounded_in_2, mem_bounded_out = self.balance_matrix_elemwise_broadcasting(gamma_matrix.copy(), 2)
+            broadcasted_shape = self.input_shape_1 if int(np.prod(np.array(self.input_shape_1[1:]))) > int(np.prod(np.array(self.input_shape_2[1:]))) else self.input_shape_2
+            branch_ratio = 1/(int(np.prod(np.array(broadcasted_shape[2:]))))
+            gamma_matrix_balanced, mem_bounded_in_1, mem_bounded_in_2, mem_bounded_out = self.balance_matrix_elemwise_broadcasting(gamma_matrix.copy(), 2, branch_ratio)
         else:
             gamma_matrix_balanced, mem_bounded_in_1, mem_bounded_in_2, mem_bounded_out = self.balance_matrix_elemwise(gamma_matrix.copy(), 2)
         if DEBUG:
@@ -272,7 +276,11 @@ class ElementWiseLayer(BaseLayer):
         rate_matrix[0, 2] = 1
         
         rate_matrix[1, 1] = 1
-        rate_matrix[1, 2] = 1
+        if self.broadcasting:
+            broadcasted_shape = self.input_shape_1 if int(np.prod(np.array(self.input_shape_1[1:]))) > int(np.prod(np.array(self.input_shape_2[1:]))) else self.input_shape_2
+            rate_matrix[1, 2] = 1/(int(np.prod(np.array(broadcasted_shape[2:]))))
+        else:
+            rate_matrix[1, 2] = 1
 
         if self.type == 'Add' or self.type == 'Mul':
             rate_matrix[2, 2] = 1
@@ -315,7 +323,7 @@ class ElementWiseLayer(BaseLayer):
                 stream_matrix[1, 2] = math.ceil(channels_2 * coarseinout)
         elif self.parrallel_dims == 'HWDC':
             if self.broadcasting:
-                stream_matrix[1, 2] = channels_2
+                stream_matrix[1, 2] = min(channels_2, math.ceil(channels_1 * depth_1 * rows_1 * cols_1 * coarseinout))
             else:
                 stream_matrix[1, 2] = math.ceil(channels_2 * depth_2 * rows_2 * cols_2 * coarseinout)
 
@@ -337,11 +345,7 @@ class ElementWiseLayer(BaseLayer):
         data_matrix[0, 2] = -1
         
         data_matrix[1, 1] = mem_bw_in_2
-        if self.broadcasting:
-            broadcasted_shape = self.input_shape_1 if int(np.prod(np.array(self.input_shape_1[1:]))) > int(np.prod(np.array(self.input_shape_2[1:]))) else self.input_shape_2
-            data_matrix[1, 2] = -1/(int(np.prod(np.array(broadcasted_shape[2:]))))
-        else:
-            data_matrix[1, 2] = -1
+        data_matrix[1, 2] = -1
 
         data_matrix[2, 2] = 1
         data_matrix[2, 3] = -mem_bw_out