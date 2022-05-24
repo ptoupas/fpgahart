ee3924ed0dff78a1ce7fe037670fc777eb76f680
Petros Toupas <ptoupas@gmail.com>
2022-05-24 22:11:43 +0300

    fix symbolic link issues

--- layer_hls_project.sh
+++ layer_hls_project.sh
@@ -6,9 +6,9 @@ LAYER_NAME="custom_conv"
 CONFIG_FILE="fpga_modeling_reports/x3d_m_seq_layers.csv"
 HLS_PARENT_DIR="/home/ptoupas/Development/hls_single_layers"
 
-python main.py $MODEL_NAME $EXECUTION_TYPE
+# python main.py $MODEL_NAME $EXECUTION_TYPE
 
-python fpgaHART/backend/generate_cpp/generate_layer.py --model_name $MODEL_NAME --prefix $LAYER_NAME --config_file $CONFIG_FILE
+# python fpgaHART/backend/generate_cpp/generate_layer.py --model_name $MODEL_NAME --prefix $LAYER_NAME --config_file $CONFIG_FILE
 
 for file in generated_data/$LAYER_NAME/*;
 do
@@ -29,7 +29,8 @@ do
 
     cp fpgaHART/backend/run_hls.tcl $HLS_PARENT_DIR/$LAYER_NAME/$CURRENT_LAYER/
     
+    FPGAHART_PATH=$PWD
     cd $HLS_PARENT_DIR/$LAYER_NAME/$CURRENT_LAYER/src
-    ln -nsf fpgaHART/backend/include_files/swish_3d.hpp swish_3d_.hpp && ln -nsf fpgaHART/backend/include_files/squeeze_3d.hpp squeeze_3d_.hpp && ln -nsf fpgaHART/backend/include_files/split_3d.hpp split_3d_.hpp && ln -nsf fpgaHART/backend/include_files/sliding_window_3d.hpp sliding_window_3d_.hpp && ln -nsf fpgaHART/backend/include_files/sigmoid_3d.hpp sigmoid_3d_.hpp && ln -nsf fpgaHART/backend/include_files/relu_3d.hpp relu_3d_.hpp && ln -nsf fpgaHART/backend/include_files/glue_3d.hpp glue_3d_.hpp && ln -nsf fpgaHART/backend/include_files/gap_3d.hpp gap_3d_.hpp && ln -nsf fpgaHART/backend/include_files/fork_3d.hpp fork_3d_.hpp && ln -nsf fpgaHART/backend/include_files/elemwise_3d.hpp elemwise_3d_.hpp && ln -nsf fpgaHART/backend/include_files/conv_3d.hpp conv_3d_.hpp && ln -nsf fpgaHART/backend/include_files/common.hpp common_.hpp && ln -nsf fpgaHART/backend/include_files/accum_3d.hpp accum_3d_.hpp
+    ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/swish_3d.hpp swish_3d_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/squeeze_3d.hpp squeeze_3d_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/split_3d.hpp split_3d_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/sliding_window_3d.hpp sliding_window_3d_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/sigmoid_3d.hpp sigmoid_3d_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/relu_3d.hpp relu_3d_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/glue_3d.hpp glue_3d_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/gap_3d.hpp gap_3d_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/fork_3d.hpp fork_3d_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/elemwise_3d.hpp elemwise_3d_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/conv_3d.hpp conv_3d_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/common.hpp common_.hpp && ln -nsf $FPGAHART_PATH/fpgaHART/backend/include_files/accum_3d.hpp accum_3d_.hpp
     cd - > /dev/null
 done
