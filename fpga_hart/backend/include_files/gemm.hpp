3b00832a5d6d049588342f08187ff5e987e9b5f4
Petros Toupas <ptoupas@gmail.com>
2022-09-04 02:24:03 +0300

    update gemm HLS code pragmas

--- fpga_hart/backend/include_files/gemm.hpp
+++ fpga_hart/backend/include_files/gemm.hpp
@@ -30,10 +30,10 @@ void gemm(
     {
         for (unsigned long feat_in = 0; feat_in < in_features; feat_in++)
         {
+#pragma HLS PIPELINE II = 1 rewind
             tmp_in = in.read();
             for (unsigned long feat_out = 0; feat_out < out_features; feat_out++)
             {
-#pragma HLS PIPELINE II = 1 rewind
                 tmp_weight = weights.read();
                 gemm_t prev = (feat_in == 0) ? gemm_t(0) : res_buffer[feat_out];
                 res_buffer[feat_out] = prev + tmp_weight * tmp_in;
