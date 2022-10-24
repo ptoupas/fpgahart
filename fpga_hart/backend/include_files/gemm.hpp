c79b52fc154cd15f0bd4f79bbe8b6e3cd1ce9e16
Petros Toupas <ptoupas@gmail.com>
2022-10-24 16:58:36 +0100

    fix in gemm module implementaion

--- fpga_hart/backend/include_files/gemm.hpp
+++ fpga_hart/backend/include_files/gemm.hpp
@@ -31,10 +31,12 @@ void gemm(
     {
         for (unsigned long feat_in = 0; feat_in < in_features; feat_in++)
         {
-            tmp_in = in.read();
             for (unsigned long feat_out = 0; feat_out < out_features; feat_out++)
             {
+#pragma HLS LOOP_FLATTEN
 #pragma HLS PIPELINE II = 1 rewind
+            	if (feat_out == 0)
+            		tmp_in = in.read();
                 tmp_weight = weights.read();
                 accum_data_t prev = (feat_in == 0) ? accum_data_t(0) : res_buffer[feat_out];
                 res_buffer[feat_out] = prev + tmp_weight * tmp_in;
