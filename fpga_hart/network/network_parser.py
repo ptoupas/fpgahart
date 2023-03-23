03cb6c5d2e7561cc904fce101261a2190540bb73
Petros Toupas <ptoupas@gmail.com>
2023-03-24 00:03:14 +0200

    add merge on initialization (still wip)

--- fpga_hart/network/network_parser.py
+++ fpga_hart/network/network_parser.py
@@ -17,8 +17,9 @@ from fpga_hart.layers.fully_connected import FCLayer
 from fpga_hart.layers.gap_3d import GAP3DLayer
 from fpga_hart.layers.pooling_3d import Pooling3DLayer
 from fpga_hart.optimizer.optimizer_helper import (
-    calculate_wr_factor, get_minimum_resource_utilization,
-    get_worst_case_buffering, get_off_chip_mem_connections, add_off_chip_connections)
+    add_off_chip_connections, calculate_wr_factor,
+    get_minimum_resource_utilization, get_off_chip_mem_connections,
+    get_worst_case_buffering)
 from fpga_hart.parser.model_descriptor import ModelLayerDescriptor
 from fpga_hart.partitions.partition_compose import PartitionComposer
 from fpga_hart.platform.platform import Platform
@@ -101,7 +102,7 @@ class NetworkParser(ModelLayerDescriptor):
             part_graph = self.create_graph(current_partition)
             partition_specs["graph"] = part_graph
             bram_util, dsp_util, layers_bram, branch_bram = self.get_partition_utilization(part_graph)
-            
+
             partition_specs["valid"] = True if bram_util <= self.config.initial_max_bram_util and dsp_util <= self.config.max_dsp_util else False
             partition_specs["weights_reloading"] = 1
 
@@ -191,33 +192,6 @@ class NetworkParser(ModelLayerDescriptor):
 
         return True
 
-    def count_layer_types(self, graph):
-
-        layers_count = dict()
-        num_layers = 0
-        for layer in nx.topological_sort(graph):
-            layer_type = graph.nodes[layer]["type"]
-            if layer_type not in ["mem_in", "mem_out"]:
-                num_layers += 1
-            if layer_type not in layers_count:
-                layers_count[layer_type] = 1
-            else:
-                layers_count[layer_type] += 1
-
-        return layers_count, num_layers
-
-    def remove_key_and_shift_dict(self, my_dict, key_to_remove, key_number):
-        num_keys = len(my_dict.keys())
-
-        del my_dict[key_to_remove]
-
-        for i in range(key_number + 1, num_keys):
-            old_key = "part_" + str(i)
-            new_key = "part_" + str(i - 1)
-            my_dict[new_key] = my_dict.pop(old_key)
-
-        return my_dict
-
     def add_key_and_shift_dict(self, my_dict, key_to_add, key_number):
         num_keys = len(my_dict.keys())
         my_dict["part_" + str(num_keys)] = dict()
@@ -251,6 +225,38 @@ class NetworkParser(ModelLayerDescriptor):
 
         return partitions
 
+    def remove_key_and_shift_dict(self, my_dict, key_to_remove, key_number):
+        num_keys = len(my_dict.keys())
+
+        del my_dict[key_to_remove]
+
+        for i in range(key_number + 1, num_keys):
+            old_key = "part_" + str(i)
+            new_key = "part_" + str(i - 1)
+            my_dict[new_key] = my_dict.pop(old_key)
+
+        return my_dict
+
+    def merge_partition(self, partitions, name, direction):
+        part_number = int(name.split("_")[-1])
+
+        if direction == "prev":
+            merge_part_number = part_number - 1
+            merge_part = f"part_{merge_part_number}"
+            if partitions[merge_part].pop("graph", None) is None:
+                raise ValueError(f"Cannot find graph for partition {merge_part}")
+            self.update_single_partition(partitions, merge_part, partitions[merge_part]["layers"] + partitions[name]["layers"])
+            self.remove_key_and_shift_dict(partitions, name, part_number)
+        elif direction == "next":
+            merge_part_number = part_number + 1
+            merge_part = f"part_{merge_part_number}"
+            if partitions[name].pop("graph", None) is None:
+                raise ValueError(f"Cannot find graph for partition {name}")
+            self.update_single_partition(partitions, name, partitions[name]["layers"] + partitions[merge_part]["layers"])
+            self.remove_key_and_shift_dict(partitions, merge_part, merge_part_number)
+
+        return partitions
+
     def update_single_partition(self, partitions, name, layers, wr_factor=1):
         if "graph" not in partitions[name]:
             graph_new = self.create_graph(layers)
@@ -270,52 +276,118 @@ class NetworkParser(ModelLayerDescriptor):
 
         return part_validity
 
-    def update_partitions(self, partitions, part_name, mode="move", direction="prev"):
-        part_number = int(part_name.split("_")[-1])
-        prev_part_name = f"part_{part_number - 1}"
-        next_part_name = f"part_{part_number + 1}"
+    def get_candidate_merge_partition(self, partitions, blacklisted_partitions):
+        for name, specs in partitions.items():
+            if specs["total_bram"] < self.config.merge_bram_threshold and name not in blacklisted_partitions:
+                return name
+        return None
 
-        match mode:
-            case "move":
-                num_layers_to_move = 2
-                if direction == "prev":
-                    print(f"Moving layers from {part_name} to previous part {prev_part_name}")
-                    prev_part_layers = partitions[prev_part_name]["layers"]
-                    curr_part_layers = partitions[part_name]["layers"]
-                    self.update_single_partition(partitions, prev_part_name, prev_part_layers + curr_part_layers[:num_layers_to_move])
-                    self.update_single_partition(partitions, part_name, curr_part_layers[num_layers_to_move:])
-                elif direction == "next":
-                    print(f"Moving layers from {part_name} to next part {next_part_name}")
-                    curr_part_layers = partitions[part_name]["layers"]
-                    next_part_layers = partitions[next_part_name]["layers"]
-                    self.update_single_partition(partitions, part_name, curr_part_layers[:-num_layers_to_move])
-                    self.update_single_partition(partitions, next_part_name, curr_part_layers[-num_layers_to_move:] + next_part_layers)
-                else:
-                    raise ValueError(f"Invalid direction {direction} for moving layers between partitions.")
-            case "merge":
-                if direction == "prev":
-                    print(f"Merging {part_name} with previous part {prev_part_name}")
-                    self.update_single_partition(partitions, prev_part_name, partitions[prev_part_name]["layers"] + partitions[part_name]["layers"])
-                    self.remove_key_and_shift_dict(partitions, part_name, part_number)
-                elif direction == "next":
-                    print(f"Merging {part_name} with next part {next_part_name}")
-                    self.update_single_partition(partitions, part_name, partitions[part_name]["layers"] + partitions[next_part_name]["layers"])
-                    self.remove_key_and_shift_dict(partitions, next_part_name, part_number + 1)
-                else:
-                    raise ValueError(f"Invalid direction {direction} for merging partitions.")
-            case "split":
-                pass
-            case _:
-                raise ValueError(f"Invalid mode {mode} for updating partitions.")
+    def get_partition_to_merge(self, partitions, name):
+        part_number = int(name.split("_")[-1])
+        part_bram_util = partitions[name]["total_bram"]
+        part_wr_factor = partitions[name]["weights_reloading"]
+        direction = None
+        fix_wr_factor = False
 
-        return partitions
+        if part_number == 0:
+            next_part_name = f"part_{part_number + 1}"
+            next_part_bram_util = partitions[next_part_name]["total_bram"]
+            next_part_wr_factor = partitions[next_part_name]["weights_reloading"]
+
+            if part_bram_util + next_part_bram_util < self.config.initial_max_bram_util + 10:
+                direction = "next"
+                if not (part_wr_factor == 1):
+                    fix_wr_factor = True
+        elif part_number == len(partitions.keys()) - 1:
+            prev_part_name = f"part_{part_number - 1}"
+            prev_part_bram_util = partitions[prev_part_name]["total_bram"]
+            prev_part_wr_factor = partitions[prev_part_name]["weights_reloading"]
+
+            if part_bram_util + prev_part_bram_util < self.config.initial_max_bram_util + 10:
+                direction = "prev"
+                if not (prev_part_wr_factor == 1):
+                    fix_wr_factor = True
+        else:
+            prev_part_name = f"part_{part_number - 1}"
+            prev_part_bram_util = partitions[prev_part_name]["total_bram"]
+            prev_part_wr_factor = partitions[prev_part_name]["weights_reloading"]
+
+            next_part_name = f"part_{part_number + 1}"
+            next_part_bram_util = partitions[next_part_name]["total_bram"]
+            next_part_wr_factor = partitions[next_part_name]["weights_reloading"]
+
+            if (part_bram_util + prev_part_bram_util < part_bram_util + prev_part_bram_util) and (part_bram_util + prev_part_bram_util < self.config.initial_max_bram_util + 10):
+                direction = "prev"
+                if not (part_wr_factor == prev_part_wr_factor):
+                    fix_wr_factor = True
+            elif (part_bram_util + next_part_bram_util < part_bram_util + prev_part_bram_util) and (part_bram_util + next_part_bram_util < self.config.initial_max_bram_util + 10):
+                direction = "next"
+                if not (part_wr_factor == next_part_wr_factor):
+                    fix_wr_factor = True
+
+        return direction, fix_wr_factor
+
+    # def count_layer_types(self, graph):
+
+    #     layers_count = dict()
+    #     num_layers = 0
+    #     for layer in nx.topological_sort(graph):
+    #         layer_type = graph.nodes[layer]["type"]
+    #         if layer_type not in ["mem_in", "mem_out"]:
+    #             num_layers += 1
+    #         if layer_type not in layers_count:
+    #             layers_count[layer_type] = 1
+    #         else:
+    #             layers_count[layer_type] += 1
+
+    #     return layers_count, num_layers
+
+    # def update_partitions(self, partitions, part_name, mode="move", direction="prev"):
+    #     part_number = int(part_name.split("_")[-1])
+    #     prev_part_name = f"part_{part_number - 1}"
+    #     next_part_name = f"part_{part_number + 1}"
+
+    #     match mode:
+    #         case "move":
+    #             num_layers_to_move = 2
+    #             if direction == "prev":
+    #                 print(f"Moving layers from {part_name} to previous part {prev_part_name}")
+    #                 prev_part_layers = partitions[prev_part_name]["layers"]
+    #                 curr_part_layers = partitions[part_name]["layers"]
+    #                 self.update_single_partition(partitions, prev_part_name, prev_part_layers + curr_part_layers[:num_layers_to_move])
+    #                 self.update_single_partition(partitions, part_name, curr_part_layers[num_layers_to_move:])
+    #             elif direction == "next":
+    #                 print(f"Moving layers from {part_name} to next part {next_part_name}")
+    #                 curr_part_layers = partitions[part_name]["layers"]
+    #                 next_part_layers = partitions[next_part_name]["layers"]
+    #                 self.update_single_partition(partitions, part_name, curr_part_layers[:-num_layers_to_move])
+    #                 self.update_single_partition(partitions, next_part_name, curr_part_layers[-num_layers_to_move:] + next_part_layers)
+    #             else:
+    #                 raise ValueError(f"Invalid direction {direction} for moving layers between partitions.")
+    #         case "merge":
+    #             if direction == "prev":
+    #                 print(f"Merging {part_name} with previous part {prev_part_name}")
+    #                 self.update_single_partition(partitions, prev_part_name, partitions[prev_part_name]["layers"] + partitions[part_name]["layers"])
+    #                 self.remove_key_and_shift_dict(partitions, part_name, part_number)
+    #             elif direction == "next":
+    #                 print(f"Merging {part_name} with next part {next_part_name}")
+    #                 self.update_single_partition(partitions, part_name, partitions[part_name]["layers"] + partitions[next_part_name]["layers"])
+    #                 self.remove_key_and_shift_dict(partitions, next_part_name, part_number + 1)
+    #             else:
+    #                 raise ValueError(f"Invalid direction {direction} for merging partitions.")
+    #         case "split":
+    #             pass
+    #         case _:
+    #             raise ValueError(f"Invalid mode {mode} for updating partitions.")
+
+    #     return partitions
 
     def refine_partitions(self, partitions):
         for part, specs in partitions.items():
             assert len(specs["layers"]) > 0, f"Partition {part} has no layers assigned to it."
             if not specs["valid"]:
                 _logger.info(f"Refining partition {part} with {len(specs['layers'])} layers and BRAM utilization {specs['total_bram']:.2f}")
-                
+
                 wr_factor = calculate_wr_factor(specs["graph"], self.config.initial_max_bram_util)
                 if wr_factor > 1:
                     _logger.info(f"WR factor for partition {part} is {wr_factor}.")
@@ -323,7 +395,7 @@ class NetworkParser(ModelLayerDescriptor):
                 else:
                     _logger.info(f"Partition {part} cannot fit in the FPGA even after WR. Splitting it into two partitions.")
                     return self.split_partition(partitions, part)
-            
+
         return partitions
 
     def parse(self):
@@ -331,7 +403,29 @@ class NetworkParser(ModelLayerDescriptor):
 
         while not self.validate_partitions(network_partitions):
             network_partitions = self.refine_partitions(network_partitions)
-            self.visualize_partitions(network_partitions)
+
+        for part, specs in network_partitions.items():
+            print(f"Partition {part} has {len(specs['layers'])} layers and BRAM utilization {specs['total_bram']:.2f}")
+
+        blacklisted_parts = []
+        cm_part = self.get_candidate_merge_partition(network_partitions, blacklisted_parts)
+        while cm_part is not None:
+            direction, fix_wr_factor = self.get_partition_to_merge(network_partitions, cm_part)
+            if direction is not None:
+                print(f"Partition {cm_part}: with {network_partitions[cm_part]['total_bram']} BRAM utilization and {len(network_partitions[cm_part]['layers'])} number of layers, wr factor {network_partitions[cm_part]['weights_reloading']}. Merging with {direction} partition with the wr factor fix = {fix_wr_factor}. Merging with {direction} partition.")
+                if fix_wr_factor:
+                    _logger.error("Fixing the WR factor is not implemented yet.")
+                    exit()
+                else:
+                    network_partitions = self.merge_partition(network_partitions, cm_part, direction)
+                    for part, specs in network_partitions.items():
+                        print(f"Partition {part} has {len(specs['layers'])} layers and BRAM utilization {specs['total_bram']:.2f}")
+            else:
+                blacklisted_parts.append(cm_part)
+            cm_part = self.get_candidate_merge_partition(network_partitions, blacklisted_parts)
+
+        assert self.validate_partitions(network_partitions), "Partitions are not valid after merging."
+        self.visualize_partitions(network_partitions)
 
         # TODO: Instead of generating completely new partitions we can have a new transform that alters a bit the existing partitions by adding or removing layers from previous or next partitions.
         # TODO: After the initialization we should do another check that searching for partitions that can be merges based on the BRAM utilization and the WR factor as well as the types of layers that exist in that partition. In order to merge it to another one we should take into account the BRAM utilization of the other partition and the WR factor.
