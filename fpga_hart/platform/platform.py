65dd177f15f76faecdd0c8a26898814d029da7c5
ptoupas <ptoupas@gmail.com>
2023-03-24 14:17:04 +0200

    update fpga devices and the way we parse their specs

--- fpga_hart/platform/platform.py
+++ fpga_hart/platform/platform.py
@@ -10,19 +10,20 @@ class Platform:
         config = configparser.ConfigParser()
         config.read(os.path.join(os.getcwd(), "fpga_hart", "config", "config_fpga.ini"))
 
-        self.word_length = int(config.get("FPGA Specifications", "word_length"))
+        self.fpga_device = config.get("FPGA Device", "fpga_device")
+
+        self.word_length = int(config.get(self.fpga_device, "word_length"))
         self.word_bytes = self.word_length / 8
-        self.clock_freq = int(config.get("FPGA Specifications", "clock_freq"))
+        self.clock_freq = int(config.get(self.fpga_device, "clock_freq"))
         self.cycles_per_sec = self.clock_freq * 1e6
-        self.bram = int(config.get("FPGA Specifications", "bram"))
-        self.bram_Kbytes = int(config.get("FPGA Specifications", "bram_type")) / 8
-        self.dsp = int(config.get("FPGA Specifications", "dsp"))
-        self.mem_bw = float(config.get("FPGA Specifications", "mem_bw"))
+        self.bram = int(config.get(self.fpga_device, "bram"))
+        self.bram_Kbytes = int(config.get(self.fpga_device, "bram_type")) / 8
+        self.dsp = int(config.get(self.fpga_device, "dsp"))
+        self.mem_bw = float(config.get(self.fpga_device, "mem_bw"))
         self.mem_bandwidth = self.mem_bw * 1e9
         self.mem_words_per_cycle = (
             self.mem_bandwidth / self.word_length
         ) / self.cycles_per_sec
-        self.fpga_device = config.get("FPGA Specifications", "fpga_device")
         self.reconfiguration_time = float(
-            config.get("FPGA Specifications", "reconfiguration_time")
+            config.get(self.fpga_device, "reconfiguration_time")
         )
