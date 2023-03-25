43c4cddbba8b5a1240a9af2b2fb7fb1f8deec1a7
Petros Toupas <ptoupas@gmail.com>
2023-03-25 11:26:41 +0200

    update bash script

--- sweep_agents.sh
+++ sweep_agents.sh
@@ -13,6 +13,6 @@ conda activate $PYTHON_ENV_NAME
 for (( c=0; c<=$NUM_AGENTS; c++ ))
 do
    echo "Starting wandb agent on exp$c screen..."
-   screen -S "exp$c" -d -m python main.py $MODEL_NAME $PLATFORM_NAME $OPTIMIZATION_TYPE $OPTIMIZATION_TARGET --enable_wandb
+   screen -S "${MODEL_NAME}-exp$c" -d -m python main.py $MODEL_NAME $PLATFORM_NAME $OPTIMIZATION_TYPE $OPTIMIZATION_TARGET --enable_wandb
    sleep 5
 done
